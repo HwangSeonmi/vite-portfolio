e8ceff574eef3efef68fc0a8cbcf8d71c63e12c5
Hwangseonmi <tjsal117@gmail.com>
2024-04-17 14:32:04 +0900

    02.intro 페이지 완료

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,20 @@
-export function intro(){
-    
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export function intro() {
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount = 15
+    let offsetValue = 150
+
+     gsap.to('.img',{
+        backgroundPosition:(-offsetValue*frameCount)+'px',
+        ease:'steps('+frameCount+')',
+        ScrollTrigger:{
+            trigger:'#intro',
+            start:'top top',
+            end:'+='+(frameCount*offsetValue),
+            pin:true,
+            scrub:true
+        }
+     })
 }
